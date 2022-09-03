9976c623fee6f19bfea8c755d3e7373c184cb9f1
AnthonyTornetta <cornchipgonecodin@gmail.com>
2022-09-03 16:31:39 -0400

    Reworking plugins that are added

--- cosmos_core/src/plugin/cosmos_core_plugin.rs
+++ cosmos_core/src/plugin/cosmos_core_plugin.rs
@@ -1,9 +1,22 @@
-use bevy::app::{App, PluginGroup, PluginGroupBuilder};
+use bevy::app::PluginGroupBuilder;
+use bevy::asset::AssetPlugin;
+use bevy::audio::AudioPlugin;
 use bevy::core::CorePlugin;
+use bevy::core_pipeline::CorePipelinePlugin;
 use bevy::diagnostic::DiagnosticsPlugin;
+use bevy::gltf::GltfPlugin;
+use bevy::input::InputPlugin;
 use bevy::log::LogPlugin;
-use bevy::prelude::*;
+use bevy::pbr::PbrPlugin;
+use bevy::prelude::{AnimationPlugin, GilrsPlugin, HierarchyPlugin, PluginGroup, TransformPlugin};
+use bevy::render::RenderPlugin;
+use bevy::scene::ScenePlugin;
+use bevy::sprite::SpritePlugin;
+use bevy::text::TextPlugin;
 use bevy::time::TimePlugin;
+use bevy::ui::UiPlugin;
+use bevy::window::WindowPlugin;
+use bevy::winit::WinitPlugin;
 use bevy_rapier3d::prelude::{NoUserData, RapierPhysicsPlugin};
 
 #[derive(Default)]
@@ -11,12 +24,24 @@ pub struct CosmosCorePluginGroup;
 
 impl PluginGroup for CosmosCorePluginGroup {
     fn build(&mut self, group: &mut PluginGroupBuilder) {
+
         group.add(LogPlugin::default());
         group.add(CorePlugin::default());
         group.add(TimePlugin::default());
         group.add(TransformPlugin::default());
         group.add(HierarchyPlugin::default());
         group.add(DiagnosticsPlugin::default());
+        group.add(InputPlugin::default());
+        group.add(WindowPlugin::default());
+
+        group.add(AssetPlugin::default());
+
+        group.add(ScenePlugin::default());
+
+        group.add(RenderPlugin::default());
+
+        group.add(CorePipelinePlugin::default());
+
         group.add(RapierPhysicsPlugin::<NoUserData>::default());
     }
 }
--- cosmos_server/src/main.rs
+++ cosmos_server/src/main.rs
@@ -1,6 +1,8 @@
+use std::net::{SocketAddr, UdpSocket};
 use std::time::SystemTime;
 use bevy::prelude::*;
-use bevy_renet::renet::{RenetServer, ServerConfig, ServerEvent};
+use bevy::winit::WinitPlugin;
+use bevy_renet::renet::{RenetConnectionConfig, RenetServer, ServerAuthentication, ServerConfig, ServerEvent};
 use bevy_renet::RenetServerPlugin;
 use cosmos_core::plugin::cosmos_core_plugin::CosmosCorePluginGroup;
 
@@ -17,7 +19,7 @@ fn handle_messages(mut server: ResMut<RenetServer>)
     }
 }
 
-fn handle_events_system(mut server_events: EventReader<ServerEvent>) {
+fn handle_events_system(mut server: ResMut<RenetServer>, mut server_events: EventReader<ServerEvent>) {
     while let Some(event) = server.get_event() {
         for event in server_events.iter() {
             match event {
@@ -38,15 +40,28 @@ fn send_message_system(mut server: ResMut<RenetServer>) {
     server.broadcast_message(channel_id, "server message".as_bytes().to_vec());
 }
 
+const PROTOCOL_ID: u64 = 7;
+
 fn main() {
-    let server = RenetServer::new();
+
+    let port: u16 = 1337;
+
+    let address: SocketAddr = format!("127.0.0.1:{}", port).parse().unwrap();
+    let socket = UdpSocket::bind(address).unwrap();
+
+    let server_config = ServerConfig::new(20, PROTOCOL_ID, address, ServerAuthentication::Unsecure);
+    let connection_config = RenetConnectionConfig::default();
+    let cur_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+
+    let server = RenetServer::new(cur_time, server_config, connection_config, socket).unwrap();
 
     App::new()
         .add_plugins(CosmosCorePluginGroup::default())
         .add_plugin(RenetServerPlugin)
+        .add_plugin(WinitPlugin::default())
         .insert_resource(server)
         .add_system(handle_messages)
         .add_system(handle_events_system)
         .add_system(send_message_system)
-    ;
+        .run();
 }
