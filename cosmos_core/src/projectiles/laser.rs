770ecf7a393ff2935730cb495c0f8c75f4214aef
AnthonyTornetta <cornchipgonecodin@gmail.com>
2023-01-12 14:23:39 -0500

    laser stuffers

--- cosmos_core/src/projectiles/laser.rs
+++ cosmos_core/src/projectiles/laser.rs
@@ -1,15 +1,22 @@
 use bevy::prelude::{
     App, Commands, Component, Entity, PbrBundle, Quat, Query, Transform, Vec3, With,
 };
-use bevy_rapier3d::prelude::CollidingEntities;
+use bevy_rapier3d::prelude::{Ccd, CollidingEntities, Sensor};
 
 #[derive(Component)]
-pub struct NoCollide(Entity);
+/// This is used to prevent the laser from colliding with the entity that fired it
+/// If this component is found on the object that it was fired on, then no collision will be registered
+pub struct NoCollide {
+    laser: Entity,
+    fired: Entity,
+}
 
 #[derive(Component)]
 pub struct Laser {
     // strength: f32,
-    active: bool, // commands despawning entity isn't instant, but changing this field is.
+    /// commands despawning entity isn't instant, but changing this field is.
+    /// Thus, this field should always be checked when determining if a laser should break/damage something.
+    active: bool,
 }
 
 /// Spawns a laser with the given position & velocity
@@ -30,20 +37,27 @@ pub fn spawn_laser(
 
     transform.look_at(velocity, Vec3::Y);
 
-    let mut ecmds = commands.spawn(PbrBundle {
+    let mut ent_cmds = commands.spawn(PbrBundle {
         transform,
         ..Default::default()
     });
 
-    ecmds
+    let laser_entity = ent_cmds.id();
+
+    ent_cmds
         .insert(Laser {
             // strength,
             active: true,
         })
+        .insert(Ccd { enabled: true })
+        .insert(Sensor)
         .insert(CollidingEntities::default());
 
     if let Some(ent) = no_collide_entity {
-        ecmds.insert(NoCollide(ent));
+        ent_cmds.insert(NoCollide {
+            fired: ent,
+            laser: laser_entity,
+        });
     }
 }
 
@@ -51,19 +65,26 @@ fn handle_events(
     mut query: Query<(Entity, Option<&NoCollide>, &mut Laser, &CollidingEntities), With<Laser>>,
     mut commands: Commands,
 ) {
-    for (entity, no_col, mut laser, c_es) in query.iter_mut() {
+    for (laser_entity, no_collide_entity, mut laser, collided_with_entities) in query.iter_mut() {
         if laser.active {
-            for ent in c_es
-                .iter()
-                .filter(|x| no_col.is_none() || *x != no_col.unwrap().0)
-            {
+            for collided_with_entity in collided_with_entities.iter() {
+                if let Some(no_collide) = no_collide_entity {
+                    if no_collide.fired == collided_with_entity && laser_entity == no_collide.laser
+                    {
+                        continue;
+                    }
+                }
+
                 if !laser.active {
                     break;
                 }
 
                 laser.active = false;
-                println!("Hit {}! Time to despawn self!", ent.index());
-                commands.entity(entity).despawn();
+                println!(
+                    "BANG! Hit {}! Time to despawn self!",
+                    collided_with_entity.index()
+                );
+                commands.entity(laser_entity).despawn();
             }
         }
     }
