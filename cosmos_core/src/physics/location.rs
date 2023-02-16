fa74859a078c737b17fa93d8900ee00e4a3ba44b
AnthonyTornetta <cornchipgonecodin@gmail.com>
2023-02-16 13:23:01 -0500

    Added location + tests for it

---
+++ cosmos_core/src/physics/location.rs
@@ -0,0 +1,133 @@
+use bevy::{
+    prelude::{App, Component, Vec3},
+    reflect::{FromReflect, Reflect},
+};
+use serde::{Deserialize, Serialize};
+
+/// This represents the diameter of a sector. So at a local
+/// of 0, 0, 0 you can travel `SECTOR_DIMENSIONS / 2.0` blocks in any direction and
+/// remain within it.
+pub const SECTOR_DIMENSIONS: f32 = 10_000.0;
+
+#[derive(Default, Component, Debug, PartialEq, Serialize, Deserialize, Reflect, FromReflect)]
+pub struct Location {
+    pub local: Vec3,
+
+    pub sector_x: i64,
+    pub sector_y: i64,
+    pub sector_z: i64,
+}
+
+impl Location {
+    pub fn new(local: Vec3, sector_x: i64, sector_y: i64, sector_z: i64) -> Self {
+        Self {
+            local,
+            sector_x,
+            sector_y,
+            sector_z,
+        }
+    }
+
+    pub fn relativie_coords_to(&self, other: &Location) -> Vec3 {
+        let (dsx, dsy, dsz) = (
+            (other.sector_x - self.sector_x) as f32,
+            (other.sector_y - self.sector_y) as f32,
+            (other.sector_z - self.sector_z) as f32,
+        );
+
+        Vec3::new(
+            SECTOR_DIMENSIONS * dsx + (other.local.x - self.local.x),
+            SECTOR_DIMENSIONS * dsy + (other.local.y - self.local.y),
+            SECTOR_DIMENSIONS * dsz + (other.local.z - self.local.z),
+        )
+    }
+}
+
+pub(crate) fn register(app: &mut App) {
+    app.register_type::<Location>();
+}
+
+#[cfg(test)]
+mod tests {
+    use bevy::prelude::Vec3;
+
+    use crate::physics::location::SECTOR_DIMENSIONS;
+
+    use super::Location;
+
+    #[test]
+    fn in_same_sector_pos() {
+        let l1 = Location::new(Vec3::new(-15.0, -15.0, -15.0), 20, -20, 20);
+        let l2 = Location::new(Vec3::new(15.0, 15.0, 15.0), 20, -20, 20);
+
+        let result = Vec3::new(30.0, 30.0, 30.0);
+
+        assert_eq!(l1.relativie_coords_to(&l2), result);
+    }
+
+    #[test]
+    fn in_same_sector_neg() {
+        let l1 = Location::new(Vec3::new(15.0, 15.0, 15.0), 20, -20, 20);
+        let l2 = Location::new(Vec3::new(-15.0, -15.0, -15.0), 20, -20, 20);
+
+        let result = Vec3::new(-30.0, -30.0, -30.0);
+
+        assert_eq!(l1.relativie_coords_to(&l2), result);
+    }
+
+    #[test]
+    fn in_diff_sector_neg() {
+        let l1 = Location::new(Vec3::new(15.0, 15.0, 15.0), 20, -20, 20);
+        let l2 = Location::new(Vec3::new(-15.0, -15.0, -15.0), 19, -21, 19);
+
+        let result = Vec3::new(
+            -30.0 - SECTOR_DIMENSIONS,
+            -30.0 - SECTOR_DIMENSIONS,
+            -30.0 - SECTOR_DIMENSIONS,
+        );
+
+        assert_eq!(l1.relativie_coords_to(&l2), result);
+    }
+
+    #[test]
+    fn in_diff_sector_pos() {
+        let l1 = Location::new(Vec3::new(15.0, 15.0, 15.0), 20, -20, 20);
+        let l2 = Location::new(Vec3::new(-15.0, -15.0, -15.0), 21, -19, 21);
+
+        let result = Vec3::new(
+            -30.0 + SECTOR_DIMENSIONS,
+            -30.0 + SECTOR_DIMENSIONS,
+            -30.0 + SECTOR_DIMENSIONS,
+        );
+
+        assert_eq!(l1.relativie_coords_to(&l2), result);
+    }
+
+    #[test]
+    fn in_far_sector_pos() {
+        let l1 = Location::new(Vec3::new(15.0, 15.0, 15.0), 20, -20, 20);
+        let l2 = Location::new(Vec3::new(-15.0, -15.0, -15.0), 30, -10, 30);
+
+        let result = Vec3::new(
+            -30.0 + SECTOR_DIMENSIONS * 10.0,
+            -30.0 + SECTOR_DIMENSIONS * 10.0,
+            -30.0 + SECTOR_DIMENSIONS * 10.0,
+        );
+
+        assert_eq!(l1.relativie_coords_to(&l2), result);
+    }
+
+    #[test]
+    fn in_far_sector_neg() {
+        let l1 = Location::new(Vec3::new(15.0, 15.0, 15.0), 20, -20, 20);
+        let l2 = Location::new(Vec3::new(-15.0, -15.0, -15.0), 10, -30, 10);
+
+        let result = Vec3::new(
+            -30.0 - SECTOR_DIMENSIONS * 10.0,
+            -30.0 - SECTOR_DIMENSIONS * 10.0,
+            -30.0 - SECTOR_DIMENSIONS * 10.0,
+        );
+
+        assert_eq!(l1.relativie_coords_to(&l2), result);
+    }
+}
--- cosmos_core/src/physics/mod.rs
+++ cosmos_core/src/physics/mod.rs
@@ -1,9 +1,11 @@
 use bevy::prelude::App;
 
 pub mod gravity_system;
+pub mod location;
 pub mod structure_physics;
 
 pub fn register(app: &mut App) {
     structure_physics::register(app);
     gravity_system::register(app);
+    location::register(app);
 }
