739e164a218ae4b01ed5b5db42d0d609a78bc094
AnthonyTornetta <cornchipgonecodin@gmail.com>
2023-04-13 19:16:46 -0400

    docs for events

--- cosmos_core/src/events/structure/change_pilot_event.rs
+++ cosmos_core/src/events/structure/change_pilot_event.rs
@@ -1,11 +1,15 @@
+//! An event that is sent when a pilot is changed
+
 use bevy::prelude::{App, Entity};
 
+/// Sent when a pilot is changed
 pub struct ChangePilotEvent {
+    /// The entity of the structure
     pub structure_entity: Entity,
-    // If this is null, the pilot is leaving
+    /// If this is null, the pilot is leaving
     pub pilot_entity: Option<Entity>,
 }
 
-pub fn register(app: &mut App) {
+pub(super) fn register(app: &mut App) {
     app.add_event::<ChangePilotEvent>();
 }
