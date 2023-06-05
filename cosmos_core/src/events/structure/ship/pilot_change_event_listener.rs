2b6c420aded640f5723f9d7282e8e0a8442dba47
AnthonyTornetta <cornchipgonecodin@gmail.com>
2023-06-05 08:38:07 -0400

    Trying to get things to work

--- cosmos_core/src/events/structure/ship/pilot_change_event_listener.rs
+++ cosmos_core/src/events/structure/ship/pilot_change_event_listener.rs
@@ -1,30 +1,24 @@
 use bevy::prelude::{
     Added, App, BuildChildren, Commands, Entity, EventReader, IntoSystemConfig, OnUpdate, Parent,
-    Quat, Query, States, Transform, Vec3, With, Without,
+    Query, States, Transform, Vec3, With, Without,
 };
 use bevy_rapier3d::prelude::{RigidBody, Sensor};
 
 use crate::entities::player::Player;
 use crate::events::structure::change_pilot_event::ChangePilotEvent;
-use crate::physics::location::Location;
+use crate::physics::location::handle_child_syncing;
 use crate::structure::ship::pilot::Pilot;
 use crate::structure::ship::Ship;
 
 fn event_listener(
     mut commands: Commands,
     mut event_reader: EventReader<ChangePilotEvent>,
-    ship_transform_query: Query<&Transform, With<Ship>>,
     mut pilot_transform_query: Query<&mut Transform, Without<Ship>>,
-    mut location_query: Query<&mut Location>,
     pilot_query: Query<&Pilot>,
 ) {
     for ev in event_reader.iter() {
         // Make sure there is no other player thinking they are the pilot of this ship
         if let Ok(prev_pilot) = pilot_query.get(ev.structure_entity) {
-            let ship_transform = ship_transform_query
-                .get(ev.structure_entity)
-                .expect("Every structure should have a transform.");
-
             commands
                 .entity(ev.structure_entity)
                 .remove_children(&[prev_pilot.entity])
@@ -32,19 +26,11 @@ fn event_listener(
 
             // The pilot may have disconnected
             if let Some(mut ec) = commands.get_entity(prev_pilot.entity) {
-                let mut location = location_query
-                    .get_mut(prev_pilot.entity)
-                    .expect("Every pilot should have a location.");
                 let mut pilot_transform = pilot_transform_query
                     .get_mut(prev_pilot.entity)
                     .expect("Every pilot should have a transform.");
 
-                let direction = ship_transform.back();
-
-                let cur_loc = *location;
-
-                location.set_from(&(cur_loc + (direction * 2.0 + Vec3::new(0.0, 1.5, 0.0))));
-                pilot_transform.rotation = Quat::IDENTITY;
+                pilot_transform.translation += Vec3::new(0.0, 1.5, -2.0);
 
                 ec.remove::<Pilot>()
                     .remove::<Parent>()
@@ -92,6 +78,7 @@ pub(super) fn register<T: States + Clone + Copy>(app: &mut App, playing_state: T
         verify_pilot_exists.in_set(OnUpdate(playing_state)),
         event_listener
             .in_set(OnUpdate(playing_state))
+            .after(handle_child_syncing)
             .after(verify_pilot_exists),
     ));
 }
