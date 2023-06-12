3db242ad1714b288556ffa41d4a04aeb4508d25e
AnthonyTornetta <cornchipgonecodin@gmail.com>
2023-06-12 18:54:03 -0400

    Fixed my stupid

--- cosmos_core/src/physics/stop_near_unloaded_chunks.rs
+++ cosmos_core/src/physics/stop_near_unloaded_chunks.rs
@@ -1,7 +1,7 @@
 //! Freezes entities that are near unloaded chunks so they don't fly into unloaded areas.
 
-use bevy::prelude::{App, Commands, Component, Entity, GlobalTransform, Query, Without};
-use bevy_rapier3d::prelude::RigidBody;
+use bevy::prelude::{App, Commands, Entity, GlobalTransform, Query, Without};
+use bevy_rapier3d::prelude::RigidBodyDisabled;
 
 use crate::{
     physics::location::SECTOR_DIMENSIONS,
@@ -16,26 +16,12 @@ use super::location::Location;
 /// At some point this may have to be based on the size of the entity. For now though, this is fine.
 const FREEZE_RADIUS: i32 = 1;
 
-/// TODO: THIS WILL NOT ALWAYS WORK
-///
-/// This has an issue where if some other system sets the rigidbody to static while this
-/// component is attached (ie a player pilots a ship while that player is frozen) then
-/// the body will be set back to dynamic once everything is loaded. This will cause in the
-/// given example the player to have a dynamic body while piloting a ship (not good).
-///
-/// Come up with a better solution at some point please.
-#[derive(Component)]
-struct NeedsSetBack;
-
 fn stop_near_unloaded_chunks(
-    mut query: Query<
-        (Entity, &Location, &mut RigidBody, Option<&NeedsSetBack>),
-        (Without<Asteroid>, Without<Planet>),
-    >,
+    query: Query<(Entity, &Location), (Without<Asteroid>, Without<Planet>)>,
     structures: Query<(Entity, &Structure, &Location, &GlobalTransform)>,
     mut commands: Commands,
 ) {
-    for (ent, loc, mut rb, needs_set_back) in query.iter_mut() {
+    for (ent, loc) in query.iter() {
         let mut is_fixed = false;
         for (structure_ent, structure, structure_loc, g_trans) in structures.iter() {
             if structure_ent == ent {
@@ -81,16 +67,14 @@ fn stop_near_unloaded_chunks(
                 });
 
             if near_unloaded_chunk {
+                commands.entity(ent).insert(RigidBodyDisabled);
                 is_fixed = true;
-                *rb = RigidBody::Fixed;
-                commands.entity(ent).insert(NeedsSetBack);
                 break;
             }
         }
 
-        if !is_fixed && needs_set_back.is_some() {
-            *rb = RigidBody::Dynamic;
-            commands.entity(ent).remove::<NeedsSetBack>();
+        if !is_fixed {
+            commands.entity(ent).remove::<RigidBodyDisabled>();
         }
     }
 }
