64a3d9a3fe8f026497921a8c36e0d4d891760bee
AnthonyTornetta <cornchipgonecodin@gmail.com>
2023-10-13 04:43:44 -0400

    Added basic volume resource

--- cosmos_client/src/music/mod.rs
+++ cosmos_client/src/music/mod.rs
@@ -1,19 +1,27 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::prelude::*;
+use bevy_kira_audio::prelude::*;
 use std::time::Duration;
-
-use bevy::prelude::{not, resource_exists, App, AssetServer, Assets, Commands, Handle, IntoSystemConfigs, Res, ResMut, Resource, Update};
-use bevy_kira_audio::{Audio, AudioControl, AudioEasing, AudioInstance, AudioPlugin, AudioTween, PlaybackState};
 
 #[derive(Resource)]
 struct BackgroundSong(Handle<AudioInstance>);
 
-pub(super) fn register(app: &mut App) {
-    app.add_plugins(AudioPlugin).add_systems(
-        Update,
-        (
-            start_playing.run_if(not(resource_exists::<BackgroundSong>())),
-            monitor_background_song.run_if(resource_exists::<BackgroundSong>()),
-        ),
-    );
+// `InspectorOptions` are completely optional
+#[derive(Reflect, Resource, InspectorOptions)]
+#[reflect(Resource, InspectorOptions)]
+pub struct VolumeSetting(#[inspector(min = 0.0, max = 1.0)] f64);
+
+impl VolumeSetting {
+    /// Returns the volume as a decimal percent [0.0, 1.0]
+    pub fn percent(&self) -> f64 {
+        self.0
+    }
+}
+
+impl Default for VolumeSetting {
+    fn default() -> Self {
+        Self(1.0)
+    }
 }
 
 fn monitor_background_song(bg_song: Res<BackgroundSong>, mut audio_instances: ResMut<Assets<AudioInstance>>, mut commands: Commands) {
@@ -27,12 +35,24 @@ fn monitor_background_song(bg_song: Res<BackgroundSong>, mut audio_instances: Re
     }
 }
 
-fn start_playing(mut commands: Commands, asset_server: Res<AssetServer>, audio: Res<Audio>) {
+fn start_playing(mut commands: Commands, asset_server: Res<AssetServer>, audio: Res<Audio>, volume: Res<VolumeSetting>) {
     let handle = audio
         .play(asset_server.load("cosmos/sounds/music/AntirockSong.ogg"))
-        .with_volume(0.15)
+        .with_volume(volume.percent())
         .fade_in(AudioTween::new(Duration::from_secs(2), AudioEasing::InOutPowi(2)))
         .handle();
 
     commands.insert_resource(BackgroundSong(handle));
+}
+
+pub(super) fn register(app: &mut App) {
+    app.add_plugins(AudioPlugin)
+        .add_systems(
+            Update,
+            (
+                start_playing.run_if(not(resource_exists::<BackgroundSong>())),
+                monitor_background_song.run_if(resource_exists::<BackgroundSong>()),
+            ),
+        )
+        .init_resource::<VolumeSetting>();
 }
