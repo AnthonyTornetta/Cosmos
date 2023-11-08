dc17f4b68cec6e9a67e041462e39ad08aa4d5a4d
Anthony Tornetta <cornchipgonecodin@gmail.com>
2023-11-08 02:22:17 -0500

    Updated commands and added some error handling crates

--- cosmos_server/src/persistence/loading.rs
+++ cosmos_server/src/persistence/loading.rs
@@ -10,7 +10,9 @@
 use std::fs;
 
 use bevy::{
-    prelude::{warn, App, Commands, Component, DespawnRecursiveExt, Entity, IntoSystemConfigs, PreUpdate, Query, Update, With, Without},
+    prelude::{
+        warn, App, Commands, Component, DespawnRecursiveExt, Entity, IntoSystemConfigs, PreUpdate, Quat, Query, Update, With, Without,
+    },
     reflect::Reflect,
 };
 use bevy_rapier3d::prelude::Velocity;
@@ -22,6 +24,17 @@ use super::{SaveFileIdentifier, SaveFileIdentifierType, SerializedData};
 #[derive(Component, Debug, Reflect)]
 /// An entity that currently has this is currently in the process of being loaded
 pub struct NeedsLoaded;
+
+#[derive(Component, Debug, Reflect)]
+/// An entity that currently has this is currently in the process of being loaded
+pub struct NeedsBlueprintLoaded {
+    /// The location this blueprint should be spawned at
+    pub spawn_at: Location,
+    /// The starting rotation of the blueprint
+    pub rotation: Quat,
+    /// The file path of the blueprint
+    pub path: String,
+}
 
 fn check_needs_loaded(query: Query<(Entity, &SaveFileIdentifier), (Without<SerializedData>, With<NeedsLoaded>)>, mut commands: Commands) {
     for (ent, nl) in query.iter() {
@@ -39,6 +52,34 @@ fn check_needs_loaded(query: Query<(Entity, &SaveFileIdentifier), (Without<Seria
         if let SaveFileIdentifierType::Base((entity_id, _, _)) = &nl.identifier_type {
             commands.entity(ent).insert(entity_id.clone());
         }
+    }
+}
+
+fn check_blueprint_needs_loaded(query: Query<(Entity, &NeedsBlueprintLoaded), Without<SerializedData>>, mut commands: Commands) {
+    for (ent, blueprint_needs_loaded) in query.iter() {
+        let path = &blueprint_needs_loaded.path;
+        let Ok(data) = fs::read(&path) else {
+            warn!("Error reading file at '{path}'. Is it there?");
+            commands.entity(ent).despawn_recursive();
+            continue;
+        };
+
+        let Ok(serialized_data) = cosmos_encoder::deserialize::<SerializedData>(&data) else {
+            warn!("Error deserializing data for {path}");
+            continue;
+        };
+
+        commands.entity(ent).insert(serialized_data);
+    }
+}
+
+/// To add your own loading event, add a system after `begin_loading` and before `done_loading`.
+pub fn begin_loading_blueprint() {}
+
+/// To add your own loading event, add a system after `begin_loading` and before `done_loading`.
+pub fn done_loading_blueprint(query: Query<Entity, With<NeedsBlueprintLoaded>>, mut commands: Commands) {
+    for ent in query.iter() {
+        commands.entity(ent).remove::<NeedsBlueprintLoaded>().remove::<SerializedData>();
     }
 }
 
@@ -69,7 +110,11 @@ fn default_load(query: Query<(Entity, &SerializedData), With<NeedsLoaded>>, mut 
 }
 
 pub(super) fn register(app: &mut App) {
-    app.add_systems(PreUpdate, check_needs_loaded)
+    app.add_systems(PreUpdate, (check_needs_loaded, check_blueprint_needs_loaded))
+        .add_systems(
+            Update,
+            (begin_loading_blueprint, done_loading_blueprint).chain().before(begin_loading),
+        )
         // Put all loading-related systems after this
         .add_systems(Update, begin_loading)
         // Put all loading-related systems before this
