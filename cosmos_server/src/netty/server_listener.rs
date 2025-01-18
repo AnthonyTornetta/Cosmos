90bf41e30490094fd39fdf5f51405eb91e547385
Anthony Tornetta <cornchipgonecodin@gmail.com>
2025-01-18 13:40:29 -0500

    Proper ship + station rotation on placement

--- cosmos_server/src/netty/server_listener.rs
+++ cosmos_server/src/netty/server_listener.rs
@@ -77,7 +77,7 @@ fn server_listen_messages(
     (mut ship_movement_event_writer, mut pilot_change_event_writer): (EventWriter<ShipSetMovementEvent>, EventWriter<ChangePilotEvent>),
     pilot_query: Query<&Pilot>,
     player_parent_location: Query<&Location, Without<Player>>,
-    mut change_player_query: Query<(&mut Transform, &mut Location, &mut PlayerLooking, &mut Velocity), With<Player>>,
+    mut q_player: Query<(&GlobalTransform, &mut Transform, &mut Location, &mut PlayerLooking, &mut Velocity), With<Player>>,
     mut build_mode: Query<&mut BuildMode>,
 
     mut send_all_chunks: ResMut<SendAllChunks>,
@@ -92,9 +92,7 @@ fn server_listen_messages(
 
                 match command {
                     ClientUnreliableMessages::PlayerBody { body, looking } => {
-                        if let Ok((mut transform, mut location, mut currently_looking, mut velocity)) =
-                            change_player_query.get_mut(player_entity)
-                        {
+                        if let Ok((_, mut transform, mut location, mut currently_looking, mut velocity)) = q_player.get_mut(player_entity) {
                             let new_loc = match body.location {
                                 NettyRigidBodyLocation::Absolute(location) => location,
                                 NettyRigidBodyLocation::Relative(rel_trans, entity) => {
@@ -219,14 +217,14 @@ fn server_listen_messages(
                         continue;
                     }
 
-                    if let Ok((transform, location, looking, _)) = change_player_query.get(client) {
+                    if let Ok((g_trans, transform, location, looking, _)) = q_player.get(client) {
                         let ship_location = *location + transform.rotation.mul_vec3(looking.rotation.mul_vec3(Vec3::new(0.0, 0.0, -4.0)));
 
                         info!("Creating ship {name}");
 
                         create_ship_event_writer.send(CreateShipEvent {
                             ship_location,
-                            rotation: looking.rotation,
+                            rotation: g_trans.rotation() * looking.rotation,
                         });
                     } else {
                         warn!("Invalid player entity - {client:?}");
@@ -234,13 +232,13 @@ fn server_listen_messages(
                 }
                 ClientReliableMessages::CreateStation { name: _name } => {
                     if let Some(client) = lobby.player_from_id(client_id) {
-                        if let Ok((transform, location, looking, _)) = change_player_query.get(client) {
+                        if let Ok((g_trans, transform, location, looking, _)) = q_player.get(client) {
                             let station_location =
                                 *location + transform.rotation.mul_vec3(looking.rotation.mul_vec3(Vec3::new(0.0, 0.0, -4.0)));
 
                             create_station_event_writer.send(CreateStationEvent {
                                 station_location,
-                                rotation: looking.rotation,
+                                rotation: g_trans.rotation() * looking.rotation,
                             });
                         }
                     }
