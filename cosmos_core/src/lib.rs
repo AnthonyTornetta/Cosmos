9b41322859b626c7a3e1b4466b7f8d7f52fb6b6a
Anthony Tornetta <cornchipgonecodin@gmail.com>
2025-02-16 20:45:02 -0500

    clippy

--- cosmos_core/src/lib.rs
+++ cosmos_core/src/lib.rs
@@ -1,6 +1,5 @@
 //! The core package contains common functionality that is shared between the client & the server.
 
-#![feature(get_many_mut)]
 #![feature(duration_constructors)]
 #![warn(missing_docs)]
 
