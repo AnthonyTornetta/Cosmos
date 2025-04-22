49031a3ae474b0a2f09eff1cc804148ebd654f16
Anthony Tornetta <cornchipgonecodin@gmail.com>
2025-04-21 20:45:45 -0400

    Removed useless param

--- cosmos_core/src/block/specific_blocks/mod.rs
+++ cosmos_core/src/block/specific_blocks/mod.rs
@@ -1,12 +1,11 @@
 //! Details about a specific type of block. For example, the logic behavior of the block.
 
-use bevy::{app::App, prelude::States};
-
+use bevy::app::App;
 
 pub mod dye_machine;
 pub mod gravity_well;
 
-pub(super) fn register<T: States + Clone + Copy>(app: &mut App, post_loading_state: T) {
+pub(super) fn register(app: &mut App) {
     gravity_well::register(app);
     dye_machine::register(app);
 }
