0d9e2a594edbf894f265de2f3d13ca1eb22c459f
AnthonyTornetta <cornchipgonecodin@gmail.com>
2025-09-22 21:13:08 -0400

    Adding display on client side

--- cosmos_client/src/block/multiblocks/shipyard/mod.rs
+++ cosmos_client/src/block/multiblocks/shipyard/mod.rs
@@ -1,10 +1,21 @@
 use bevy::prelude::*;
 use cosmos_core::{
     block::multiblock::prelude::{ClientFriendlyShipyardState, ShowShipyardUi},
+    inventory::Inventory,
+    item::{Item, usable::blueprint::BlueprintItemData},
+    netty::client::LocalPlayer,
     prelude::{Structure, StructureBlock},
+    registry::identifiable::Identifiable,
 };
 
-use crate::ui::{OpenMenu, components::window::GuiWindow};
+use crate::{
+    inventory::InventoryNeedsDisplayed,
+    ui::{
+        OpenMenu,
+        components::{button::CosmosButton, window::GuiWindow},
+        item_renderer::RenderItem,
+    },
+};
 
 fn on_open_shipyard(
     q_structure: Query<&Structure>,
@@ -23,7 +34,23 @@ fn on_open_shipyard(
     let state = structure.query_block_data(ev.shipyard_block.coords(), &q_shipyard_state);
 }
 
-fn create_shipyard_ui(commands: &mut Commands, state: Option<&ClientFriendlyShipyardState>, block: StructureBlock) {
+fn create_shipyard_ui(
+    commands: &mut Commands,
+    state: Option<&ClientFriendlyShipyardState>,
+    block: StructureBlock,
+    inventory: &Inventory,
+    q_blueprint_data: &Query<&BlueprintItemData>,
+    blueprint: &Item,
+    q_inventory: Query<Entity, With<LocalPlayer>>,
+) {
+    let Ok(inv) = q_inventory.single() else {
+        return;
+    };
+
+    commands
+        .entity(inv)
+        .insert(InventoryNeedsDisplayed::Normal(crate::inventory::InventorySide::Left));
+
     commands
         .spawn((
             OpenMenu::new(0),
@@ -52,7 +79,26 @@ fn create_shipyard_ui(commands: &mut Commands, state: Option<&ClientFriendlyShip
         ))
         .with_children(|p| match state {
             None => {
-                p.spawn((Text::new("Select Blueprint")));
+                p.spawn((Text::new("Insert Blueprint")));
+
+                for bp in inventory
+                    .iter()
+                    .flatten()
+                    .filter(|i| i.item_id() == blueprint.id())
+                    .flat_map(|i| i.data_entity().and_then(|e| q_blueprint_data.get(e).ok()))
+                {
+                    p.spawn((
+
+                    ))
+                    p.spawn((
+                        RenderItem { item_id: blueprint.id() },
+                        Node {
+                            width: Val::Px(64.0),
+                            height: Val::Px(64.0),
+                            ..Default::default()
+                        },
+                    ));
+                }
             }
             Some(ClientFriendlyShipyardState::Paused(p)) => {}
             Some(ClientFriendlyShipyardState::Building(b)) => {}
