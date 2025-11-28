8864f8452abd84cad50527797e209b4e55385d72
Anthony Tornetta <cornchipgonecodin@gmail.com>
2025-11-27 21:47:43 -0500

    Creating run scripts

---
+++ scripts/bat/run-client.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+:: Gets working dir
+set HERE=%~dp0
+
+:: Add this directory to the PATH so windows can find DLLs
+set PATH=%HERE%;%PATH%
+
+"%HERE%cosmos_client.exe" %*
+
+endlocal
