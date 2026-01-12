2e4862181252a0fda9e485a0ffe67692f8c851ca
Anthony Tornetta <cornchipgonecodin@gmail.com>
2026-01-12 02:07:35 -0500

    Adding new power category

--- cosmos_server/src/items/categories.rs
+++ cosmos_server/src/items/categories.rs
@@ -2,11 +2,12 @@ use bevy::prelude::*;
 use cosmos_core::{item::item_category::ItemCategory, registry::Registry, state::GameState};
 
 fn create_item_categories(mut reg: ResMut<Registry<ItemCategory>>) {
-    reg.register(ItemCategory::new("cosmos:hull", "cosmos:ship_hull_dark_grey"));
+    reg.register(ItemCategory::new("cosmos:build_blocks", "cosmos:ship_hull_dark_grey"));
     reg.register(ItemCategory::new("cosmos:weapons", "cosmos:missile_launcher"));
     reg.register(ItemCategory::new("cosmos:utility", "cosmos:ship_core"));
     reg.register(ItemCategory::new("cosmos:logic", "cosmos:and_gate"));
     reg.register(ItemCategory::new("cosmos:material", "cosmos:iron_bar"));
+    reg.register(ItemCategory::new("cosmos:power", "cosmos:energy_cell"));
 }
 
 pub(super) fn register(app: &mut App) {
