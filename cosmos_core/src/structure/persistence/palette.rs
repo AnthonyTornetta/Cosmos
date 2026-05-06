391b21f105f15db01008d3e5376b74226c03a5cb
Anthony Tornetta <cornchipgonecodin@gmail.com>
2026-05-05 23:46:21 -0400

    Docs

--- cosmos_core/src/structure/persistence/palette.rs
+++ cosmos_core/src/structure/persistence/palette.rs
@@ -1,3 +1,5 @@
+//! Used for mapping `numeric id` -> `string id` so that serialized data stays consistent across
+//! numeric ID reordering.
 
 use bevy::{platform::collections::HashMap, prelude::*};
 use itertools::Itertools;
@@ -13,6 +15,8 @@ use crate::{
 };
 
 #[derive(Serialize, Deserialize, Reflect, Clone, Debug, Component)]
+/// Used for mapping `numeric id` -> `string id` so that serialized data stays consistent across
+/// numeric ID reordering.
 pub struct Palette<T: Identifiable + std::fmt::Debug> {
     mappings: HashMap<u16, String>,
     #[serde(skip)]
@@ -38,10 +42,12 @@ impl<T: Identifiable + std::fmt::Debug> Default for Palette<T> {
 }
 
 impl Palette<Item> {
+    /// Creates a new [`Palette<Item>`] for these itemstacks
     pub fn new_from_itemstacks<'a, I: Iterator<Item = &'a ItemStack>>(itemstacks: I, items: &Registry<Item>) -> Self {
         Self::new_from_items(itemstacks.map(|x| items.from_numeric_id(x.item_id())))
     }
 
+    /// Creates a new [`Palette<Item>`] for these items
     pub fn new_from_items<'a, I: Iterator<Item = &'a Item>>(items: I) -> Self {
         Self::new(
             items
@@ -51,12 +57,14 @@ impl Palette<Item> {
         )
     }
 
+    /// Creates a new [`Palette<Item>`] for this inventory
     pub fn new_from_inventory(inventory: &Inventory, items: &Registry<Item>) -> Self {
         Self::new_from_itemstacks(inventory.iter().flatten(), items)
     }
 }
 
 impl Palette<Block> {
+    /// Creates a new [`Palette<Block>`] for this structure
     pub fn new_from_structure(structure: &Structure, blocks: &Registry<Block>) -> Self {
         Self::new(
             structure
@@ -70,6 +78,7 @@ impl Palette<Block> {
 }
 
 impl<T: Identifiable + std::fmt::Debug> Palette<T> {
+    /// Creates a new generic [`Palette`] with these mappigns
     pub fn new(mappings: HashMap<u16, String>) -> Self {
         Self {
             mappings,
@@ -77,15 +86,19 @@ impl<T: Identifiable + std::fmt::Debug> Palette<T> {
         }
     }
 
+    /// Checks if this id exists within this
     pub fn exists(&self, id: u16) -> bool {
         self.mappings.contains_key(&id)
     }
 
+    /// Gets the stored unlocalized name for this id
     pub fn get(&self, id: u16) -> Option<&String> {
         self.mappings.get(&id)
     }
 
-    pub fn compute_cache(&mut self, registry: &Registry<T>) -> Option<Vec<String>> {
+    /// Pre-computes all `numeric id` -> `T` combinations. You don't need to manually call this, as
+    /// it is automatically computed when [`Self::get_cached`] is called.
+    fn compute_cache(&mut self, registry: &Registry<T>) -> Option<Vec<String>> {
         let mut missing: Vec<String> = vec![];
         for (id, name) in self.mappings.iter() {
             let Some(item) = registry.from_id(name) else {
@@ -102,15 +115,19 @@ impl<T: Identifiable + std::fmt::Debug> Palette<T> {
         None
     }
 
+    /// Gets the `T` for this id from a pre-computed cache of mappings. If the cache has not yet
+    /// been computed, it is computed now.
     pub fn get_cached(&mut self, id: u16, registry: &Registry<T>) -> Option<&T> {
         if self.cache.is_empty()
-            && let Some(missing) = self.compute_cache(registry) {
-                error!("Invalid ids: {missing:?} in palette {self:?}");
-            }
+            && let Some(missing) = self.compute_cache(registry)
+        {
+            error!("Invalid ids: {missing:?} in palette {self:?}");
+        }
 
         self.cache.get(&id)
     }
 
+    /// Sets the mapping for this numeric id to be this unlocalized name
     pub fn set(&mut self, id: u16, str_id: String) {
         self.mappings.insert(id, str_id);
     }
